94c07da10ee6869fdbe0bc37943b5c292f37052e
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-25 14:27:57 +0530

    Update Matrix.java

--- Matrix.java
+++ Matrix.java
@@ -47,3 +47,32 @@ public class Matrix {
         }
     }
 }
+
+#####
+
+OUTPUT:
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=12246:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java java\out\production\java java" Matrix
+Enter the rows and colums of first matrix:
+2
+2
+Elemnts of the first matrix:
+1 2
+3 4
+Enter the number of rows and colums of second matrix:
+2 
+2
+Elemnts of the second matrix:
+9 0
+56 1
+Sum of the two matrices:
+10 2 
+59 5 
+
+Process finished with exit code 0
+
+    
+    
+#####
+
+
+
