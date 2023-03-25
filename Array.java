26b6ecb1885793e284b02fe7c39c7a814e672eae
ashmistalin <103128410+ashmistalin@users.noreply.github.com>
2023-03-25 14:28:54 +0530

    Update Array.java

--- Array.java
+++ Array.java
@@ -17,3 +17,19 @@ public class Array {
         }
     }
 }
+
+
+######
+
+OUTPUT:
+
+C:\Users\Dell\.jdks\openjdk-19.0.2\bin\java.exe "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\lib\idea_rt.jar=12337:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2022.3.3\bin" -Dfile.encoding=UTF-8 -Dsun.stdout.encoding=UTF-8 -Dsun.stderr.encoding=UTF-8 -classpath "C:\Users\Dell\IdeaProjects\java java\out\production\java java" Array
+Original array:
+1 2 3 4 5 
+
+Copied array:
+1 2 3 4 5 
+Process finished with exit code 0
+
+    
+######
